3c4682064e9cdf991783ed29e4bf7fab55345f59
Ag <70638763+choeun01@users.noreply.github.com>
2024-02-15 20:15:28 +0900

    [Feat : front] 현재 인원 휴먼 인원 버튼 구현
    - 각각 해당하는 것을 누르면 해당하는 정보를 보여주도록 하는 버튼을 구현했습니다.

--- frontend/src/app/club/[id]/page.tsx
+++ frontend/src/app/club/[id]/page.tsx
@@ -11,6 +11,7 @@ import NavigationFooter from '@/atoms/molecule/navigation-footer';
 export default function Main() {
   const router = useRouter();
   const [token, setToken] = useRecoilState(userToken);
+  const [type, setType] = useState(0);
   const [isKing, setIsKing] = useRecoilState(isNuriKing);
 
   return (
@@ -18,7 +19,18 @@ export default function Main() {
       <header>
         <Header isVisible={false} />
       </header>
-
+      <section>
+        <div className="mx-[7.5%] grid grid-cols-2 mb-[2rem]">
+          <div className={`text-center font-bold text-[1.5rem] ${type === 0 ? 'text-black' : 'text-grey'}`} onClick={() => setType(0)}>
+            현재 인원
+          </div>
+          <div className={`text-center font-bold text-[1.5rem] ${type === 1 ? 'text-black' : 'text-grey'}`} onClick={() => setType(1)}>
+            휴먼 인원
+          </div>
+          <div className={`border-t-2 mt-[1rem] ${type === 0 ? 'border-black' : 'border-grey'}`}></div>
+          <div className={`border-t-2 mt-[1rem] ${type === 1 ? 'border-black' : 'border-light-grey'}`}></div>
+        </div>
+      </section>
       <NavigationFooter isKing={isKing}></NavigationFooter>
     </main>
   );
