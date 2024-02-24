4401a26346df0f3be50783b24a53d95f0b887875
Ag <70638763+choeun01@users.noreply.github.com>
2024-02-24 15:57:49 +0900

    [Feat : front] LongThinButton 구현
    - LongThinButton을 구현하였습니다.

---
+++ frontend/src/atoms/atom/LongThinButton.tsx
@@ -0,0 +1,8 @@
+type data = {
+  text: string;
+  addClass?: string;
+};
+
+export default function MiddleButton({ text, addClass }: data) {
+  return <button className={`w-[17.5rem] h-[1.88rem] bg-blue rounded-[0.63rem] text-white font-bold ${addClass}`}>{text}</button>;
+}
