ae1a7acca2f612039052badc5586021612d3357c
Ag <70638763+choeun01@users.noreply.github.com>
2024-02-24 17:40:10 +0900

    [Feat : front] 회원 상세 조회 모달창 구현
    - 회원 상세 페이지에서 회원을 클릭하면 회원이 나타나도록 하였습니다.
    - 팀장일 경우 추방, 휴면 전환, 휴가 부여를 할 수 있도록 하였습니다.

--- frontend/src/atoms/molecule/MemberInformationModal.tsx
+++ frontend/src/atoms/molecule/MemberInformationModal.tsx
@@ -1,12 +1,15 @@
 import { axAuth } from '@/apis/axiosinstance';
-import Button from '../atom/thin-long-button';
+import LongThinButton from '../atom/LongThinButton';
+import ShortButton from '../atom/ShortButton';
 import { useRecoilState } from 'recoil';
 import { userToken } from '../../states/index';
 import { useEffect, useState } from 'react';
 import { data } from 'autoprefixer';
 
 type props = {
-  userId: number;
+  clubId: string | undefined;
+  clubMemberId: number;
+  vacationToken: string;
   setIsMemberInfoOpen: (isOpen: number) => void;
   isKing: boolean;
   type: number;
@@ -16,15 +19,10 @@ type data = {
   name: string;
   major: string;
   studentId: string;
-  phoneNum: string;
-  hobby: string;
-  specialtySkill: string;
-  mbti: string;
-  nuriKing: boolean;
-  email: string;
+  selfIntroduction: string;
 };
 
-export default function MemberInformationModal({ userId, setIsMemberInfoOpen, isKing, type }: props) {
+export default function MemberInformationModal({ clubId, clubMemberId, vacationToken, setIsMemberInfoOpen, isKing, type }: props) {
   const [token, setToken] = useRecoilState(userToken);
   const [userInfo, setUserInfo] = useState<data>();
   const [vacationCount, setVacationCount] = useState(0);
@@ -40,30 +38,53 @@ export default function MemberInformationModal({ userId, setIsMemberInfoOpen, is
   useEffect(() => {
     axAuth(token)({
       method: 'get',
-      url: `/main/detail/${userId}`,
+      url: `/clubs/informations/${clubId}/details/${clubMemberId}`,
     })
       .then(res => {
-        // console.log(res.data);
-        setUserInfo(res.data);
+        setUserInfo(res.data.result);
       })
       .catch(err => {
         console.log(err);
       });
   }, []);
 
+  const giveExpulsion = () => {
+    axAuth(token)({
+      method: 'post',
+      url: `/clubs/informations/${clubId}/details/${clubMemberId}/expulsion`,
+    })
+      .then(res => {
+        handleModal();
+      })
+      .catch(err => {
+        handleModal();
+      });
+  };
+
+  const giveDormancy = () => {
+    axAuth(token)({
+      method: 'post',
+      url: `/clubs/informations/${clubId}/details/${clubMemberId}/dormancy`,
+    })
+      .then(res => {
+        handleModal();
+      })
+      .catch(err => {
+        handleModal();
+      });
+  };
+
   const giveVacation = () => {
     axAuth(token)({
       method: 'post',
-      url: 'vacations/count',
+      url: `/clubs/informations/${clubId}/details/${clubMemberId}/vacation`,
       data: {
-        vacationCount: vacationCount,
-        userId: userId,
+        vacationToken: vacationCount,
       },
     })
       .then(res => {
         setVacationCount(0);
         handleModal();
-        console.log(res.data);
       })
       .catch(err => {
         setVacationCount(0);
@@ -73,44 +94,49 @@ export default function MemberInformationModal({ userId, setIsMemberInfoOpen, is
 
   return (
     <div className="absolute z-10 w-[100vw] h-[100vh] bg-black/60 flex justify-center items-center" onClick={handleModal}>
-      <div className="bg-white w-[85%] rounded-[4%] flex flex-col p-[1rem]" onClick={e => e.stopPropagation()}>
+      <div className="bg-white w-[20rem] h-[24rem] rounded-[4%] flex flex-col p-[1rem]" onClick={e => e.stopPropagation()}>
         {userInfo ? (
           <>
-            <div className="font-bold text-xl text-left mb-[1rem]">{userInfo.name}</div>
-            <div className="flex place-content-between">
+            <div className="flex items-center font-bold text-left mb-[1rem]">
+              <div className="text-xl mr-[3rem]">{userInfo.name}</div>
+              {isKing && type == 0 ? (
+                <>
+                  <div className="mr-[1rem]" onClick={giveExpulsion}>
+                    <ShortButton text="추방하기" addClass="bg-red"></ShortButton>
+                  </div>
+                  <div onClick={giveDormancy}>
+                    <ShortButton text="휴먼 전환" addClass="bg-orange"></ShortButton>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            <div className="flex place-content-between mb-[0.2rem]">
               <span className="font-semibold text-base">전공: {userInfo.major}</span>
               <span className="font-semibold text-base">학번: {userInfo.studentId}</span>
             </div>
-            <div className="border border-light-grey mt-[0.3rem]"></div>
-            <span className="font-semibold text-base">핸드폰 번호: {userInfo.phoneNum}</span>
-            <div className="border border-light-grey mt-[0.3rem]"></div>
-            <div className="flex place-content-between">
-              <span className="font-semibold text-base">취미: {userInfo.hobby}</span>
-              <span className="font-semibold text-base mr-5">특기: {userInfo.specialtySkill}</span>
-            </div>
-            <div className="border border-light-grey mt-[0.3rem]"></div>
-            <div className="flex place-content-between">
-              <span className={`font-semibold text-base ${isKing && type == 0 ? null : 'mb-[1rem]'}`}>MBTI: {userInfo.mbti}</span>
-              <span className={`font-semibold text-base ${isKing && type == 0 ? null : 'mb-[1rem]'}`}>email : {userInfo.email}</span>
+            <div className="font-semibold text-base mb-[0.2rem]">자기소개</div>
+            <div className="w-[17.81rem] h-[9.69rem] border border-grey rounded-[0.63rem] p-[0.5rem] mb-[1rem]">
+              <span className="font-semibold text-base">{userInfo.selfIntroduction}</span>
             </div>
             {isKing && type == 0 ? (
               <>
-                <div className="border border-light-grey my-[0.3rem]"></div>
-                <div className="mb-auto">
-                  <div>
+                <div>
+                  <div className="font-semibold text-base">남은 휴가 : {vacationToken}일</div>
+                  <div className="flex items-center mb-[1rem]">
                     <span className="font-semibold text-base">휴가 제공 : </span>
-                    <input type="number" className="border w-[7rem] mr-[1rem] mb-[1rem]" onChange={inputVacation} />
-                    <button className="bg-red w-[4rem] rounded-[10%]" onClick={giveVacation}>
-                      부여하기
-                    </button>
+                    <input type="number" className="border w-[5.56rem] h-[1.81rem] m-[0.2rem]" onChange={inputVacation} />
+                    <span className="font-semibold text-base mr-[0.5rem]">일</span>
+                    <div onClick={giveVacation}>
+                      <ShortButton text="부여하기" addClass="bg-red"></ShortButton>
+                    </div>
                   </div>
                 </div>
               </>
             ) : null}
           </>
         ) : null}
-        <div className="w-[100%] flex justify-center" onClick={handleModal}>
-          <Button text="확인" addClass="text-sm" />
+        <div className="flex justify-center mt-auto" onClick={handleModal}>
+          <LongThinButton text={'확인'} />
         </div>
       </div>
     </div>
