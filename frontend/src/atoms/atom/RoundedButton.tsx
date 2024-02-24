b5a64e2fe74ca1303ddb1ff9acd22d176d52912c
Ag <70638763+choeun01@users.noreply.github.com>
2024-02-24 17:53:40 +0900

    [Refactor : front] rounded button 메소드 명 수정
    -  메소드 명이 제대로  되도록 수정하였습니다.

--- frontend/src/atoms/atom/RoundedButton.tsx
+++ frontend/src/atoms/atom/RoundedButton.tsx
@@ -3,6 +3,6 @@ type data = {
   addClass?: string;
 };
 
-export default function SmallButton({ text, addClass }: data) {
+export default function RoundedButton({ text, addClass }: data) {
   return <button className={`w-[7.31rem] h-[2.06rem] bg-blue rounded-[0.63rem] text-white font-bold ${addClass}`}>{text}</button>;
 }
