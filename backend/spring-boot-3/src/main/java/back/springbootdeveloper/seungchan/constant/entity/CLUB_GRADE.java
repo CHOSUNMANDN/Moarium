d49aa306d0b1d601ba83c3bb8a41f042692342fd
박승찬 <seungchan141414@gmail.com>
2024-03-10 17:04:31 +0900

    [Feat : back] TokenService에 실장 검증 메서드 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/entity/CLUB_GRADE.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/entity/CLUB_GRADE.java
@@ -4,18 +4,35 @@ import lombok.Getter;
 
 @Getter
 public enum CLUB_GRADE {
-    LEADER(1, "LEADER"), DEPUTY_LEADER(2, "DEPUTY_LEADER"),
-    MEMBER(3, "MEMBER"), DORMANT(4, "DORMANT");
+  LEADER(1, "LEADER"), DEPUTY_LEADER(2, "DEPUTY_LEADER"),
+  MEMBER(3, "MEMBER"), DORMANT(4, "DORMANT");
 
-    private Integer id;
-    private String grade;
+  private Integer id;
+  private String grade;
 
-    CLUB_GRADE(Integer id, String grade) {
-        this.id = id;
-        this.grade = grade;
-    }
+  CLUB_GRADE(Integer id, String grade) {
+    this.id = id;
+    this.grade = grade;
+  }
 
-    public boolean is(CLUB_GRADE targetClubGrade) {
-        return this.grade.equals(targetClubGrade.getGrade());
-    }
+  /**
+   * 현재 ClubGrade의 등급과 주어진 targetClubGrade의 등급을 비교하여 동일한지 여부를 반환합니다.
+   *
+   * @param targetClubGrade 비교할 ClubGrade 객체
+   * @return 현재 ClubGrade의 등급과 주어진 targetClubGrade의 등급이 동일하면 true, 아니면 false
+   */
+  public boolean is(CLUB_GRADE targetClubGrade) {
+    return this.grade.equals(targetClubGrade.getGrade());
+  }
+
+
+  /**
+   * 현재 ClubGrade의 id와 주어진 targetClubGradeId를 비교하여 동일한지 여부를 반환합니다.
+   *
+   * @param targetClubGradeId 비교할 ClubGrade의 id
+   * @return 현재 ClubGrade의 id와 주어진 targetClubGradeId가 동일하면 true, 아니면 false
+   */
+  public boolean is(Long targetClubGradeId) {
+    return this.id.equals(targetClubGradeId.intValue());
+  }
 }
