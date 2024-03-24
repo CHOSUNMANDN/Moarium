13a5e3a3b2eb2d72d18bc642e9d2adca879878d2
박승찬 <seungchan141414@gmail.com>
2024-03-24 15:20:36 +0900

    [Feat : front] Small Circle 컴포넌트 등록

--- frontend/src/atoms/atom/text-area-form-value.tsx
+++ frontend/src/atoms/atom/text-area-form-value.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import SmallCircle from './small-circle';
 
 type data = {
   title: string;
@@ -7,7 +8,7 @@ type data = {
   value?: string;
 };
 
-export default function TextAreaForm({ title, userData, setUserData, dataname, placeholder, value }: data & { userData: any; setUserData: any }) {
+export default function TextAreaForm({ title, setUserData, dataname, placeholder, value }: data & { userData: any; setUserData: any }) {
   const [textLength, setTextLength] = useState(0);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,7 +20,7 @@ export default function TextAreaForm({ title, userData, setUserData, dataname, p
   return (
     <div className="w-[100%] mb-[0.5rem] relative">
       <label htmlFor={dataname} className="font-bold">
-        <span className="text-[#FF0A0A]">* </span>
+        <SmallCircle></SmallCircle>
         {title}
       </label>
       <span className="font-semibold absolute right-[1rem]">{textLength}자</span>
