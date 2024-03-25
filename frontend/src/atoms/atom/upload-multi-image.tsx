ca2f8b16132b9c03bfa6784b86a9a6ba16f637d0
박승찬 <seungchan141414@gmail.com>
2024-03-26 02:51:55 +0900

    [Feat : front] multi image 이미지 미리보기 기능 등록

--- frontend/src/atoms/atom/upload-multi-image.tsx
+++ frontend/src/atoms/atom/upload-multi-image.tsx
@@ -7,11 +7,33 @@ type dataType = {
 };
 
 export default function UploadMultiImage(data: dataType & { userData: any; setUserData: any }) {
-  const onChangeOneImg = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
+  const [previewImages, setPreviewImages] = useState<any[]>([]);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const uploadFile = e.target.files;
     if (uploadFile) {
       data.setUserData((prevData: any) => ({ ...prevData, [data.dataname]: uploadFile }));
+    }
+
+    if (uploadFile) {
+      setPreviewImages([]);
+
+      for (let i = 0; i < uploadFile.length; i++) {
+        const file = uploadFile[i];
+        const fReader = new FileReader();
+        fReader.readAsDataURL(file);
+
+        fReader.onloadend = event => {
+          const lastModified = file.lastModified;
+          setPreviewImages(prevImages => [
+            ...prevImages,
+            {
+              id: lastModified,
+              src: event.target?.result,
+            },
+          ]);
+        };
+      }
     }
   };
 
@@ -20,7 +42,20 @@ export default function UploadMultiImage(data: dataType & { userData: any; setUs
       <label htmlFor={data.dataname} className="font-bold">
         {data.title}
       </label>
-      <input type="file" id="profile-upload" accept="image/*" multiple onChange={onChangeOneImg} />
+      <div id="current_file">
+        <input type="file" id="profile-upload" accept="image/*" multiple onChange={handleImageChange} />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="subject">사진 미리보기</label>
+        <div id="preview_img">
+          {previewImages.map(image => (
+            <div key={image.id} className="img_div relative">
+              <img src={image.src} className="img_div_img" />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
