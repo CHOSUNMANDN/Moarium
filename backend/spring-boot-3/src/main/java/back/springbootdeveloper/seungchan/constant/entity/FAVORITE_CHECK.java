a571551f16d15a437cdd178b422b21a6eb4e4403
박승찬 <seungchan141414@gmail.com>
2024-03-26 23:36:59 +0900

    [Feat : back] Enum 매개 변수 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/entity/FAVORITE_CHECK.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/entity/FAVORITE_CHECK.java
@@ -1,5 +1,14 @@
 package back.springbootdeveloper.seungchan.constant.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum FAVORITE_CHECK {
-  CHECK, UNCHECK
+  CHECK("CHECK"), UNCHECK("UN_CHECK");
+
+  private String state;
+
+  FAVORITE_CHECK(final String state) {
+    this.state = state;
+  }
 }
