96d043c8dce32ecca96b845b083b2d5e4916fb64
박승찬 <seungchan141414@gmail.com>
2024-03-27 21:54:21 +0900

    [Test : back] 메인 페이지 조회 테스트

---
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/MainControllerTest.java
@@ -0,0 +1,64 @@
+package back.springbootdeveloper.seungchan.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import back.springbootdeveloper.seungchan.annotation.MoariumSpringBootTest;
+import back.springbootdeveloper.seungchan.constant.entity.CLUB_GRADE;
+import back.springbootdeveloper.seungchan.constant.entity.FAVORITE_CHECK;
+import back.springbootdeveloper.seungchan.dto.response.ClubFindInformation;
+import back.springbootdeveloper.seungchan.entity.Club;
+import back.springbootdeveloper.seungchan.entity.ClubMember;
+import back.springbootdeveloper.seungchan.entity.ClubMemberInformation;
+import back.springbootdeveloper.seungchan.entity.Member;
+import back.springbootdeveloper.seungchan.filter.exception.judgment.EntityNotFoundException;
+import back.springbootdeveloper.seungchan.repository.ClubMemberInformationRepository;
+import back.springbootdeveloper.seungchan.repository.ClubMemberRepository;
+import back.springbootdeveloper.seungchan.repository.ClubRepository;
+import back.springbootdeveloper.seungchan.repository.MemberRepository;
+import back.springbootdeveloper.seungchan.service.ImageService;
+import back.springbootdeveloper.seungchan.testutil.TestCreateUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@MoariumSpringBootTest
+class MainControllerTest {
+
+  @Autowired
+  protected ObjectMapper objectMapper;
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private TestCreateUtil testCreateUtil;
+  @Autowired
+  private ClubMemberRepository clubMemberRepository;
+  @Autowired
+  private ClubRepository clubRepository;
+  @Autowired
+  private MemberRepository memberRepository;
+  @Autowired
+  private ImageService imageService;
+  @Autowired
+  private ClubMemberInformationRepository clubMemberInformationRepository;
+  private Member memberOneClubLeader;
+  private Long targetClubOneId;
+  private String token;
+
+  @BeforeEach
+  void setUp() {
+    memberOneClubLeader = testCreateUtil.get_entity_one_club_leader_member();
+    targetClubOneId = testCreateUtil.getONE_CLUB_ID();
+  }
+
+
+}
