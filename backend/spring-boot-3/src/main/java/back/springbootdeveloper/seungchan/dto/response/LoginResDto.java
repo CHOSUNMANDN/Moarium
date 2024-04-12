abe846c8a4fc77184326455e5dc400cd5aa0da92
박승찬 <seungchan141414@gmail.com>
2024-04-12 15:07:43 +0900

    [Feat : back] 구글 로그인 사용시 기존 멤버, 새로운 맴버 구분 응답 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/LoginResDto.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/LoginResDto.java
@@ -1,15 +1,23 @@
 package back.springbootdeveloper.seungchan.dto.response;
 
+import back.springbootdeveloper.seungchan.constant.entity.CHECK;
+import back.springbootdeveloper.seungchan.constant.judgement.MEMBERSHIP_STATUS;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
 @Data
 public class LoginResDto {
 
   private String accessToken;
+  private String memberStatus;
 
   public LoginResDto(String accessToken) {
+    this.memberStatus = MEMBERSHIP_STATUS.MEMBER.getState();
     this.accessToken = accessToken;
+  }
+
+  public LoginResDto(MEMBERSHIP_STATUS membershipStatus) {
+    this.memberStatus = MEMBERSHIP_STATUS.NON_MEMBER.getState();
+    this.accessToken = "";
   }
 }
