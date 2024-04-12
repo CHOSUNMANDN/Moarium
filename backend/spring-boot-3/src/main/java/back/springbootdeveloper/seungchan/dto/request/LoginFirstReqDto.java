dc0b6d9b7f91918af9ff90d20d57f04928b0987a
박승찬 <seungchan141414@gmail.com>
2024-04-12 18:50:49 +0900

    [Feat : back] Valid 검증 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/LoginFirstReqDto.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/LoginFirstReqDto.java
@@ -1,6 +1,8 @@
 package back.springbootdeveloper.seungchan.dto.request;
 
 import back.springbootdeveloper.seungchan.entity.Member;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,16 @@ import lombok.NoArgsConstructor;
 public class LoginFirstReqDto {
 
   private final Integer FIRST_INDEX = 0;
+  @NotBlank(message = "{validation.name.notblank}")
   private String name;
+  @NotBlank(message = "{validation.nickName.notblank}")
   private String nickName;
+  @NotBlank(message = "{validation.email.notblank}")
+  @Email(message = "{validation.email.invalid}")
   private String email;
+  @NotBlank(message = "{validation.major.notblank}")
   private String major;
+  @NotBlank(message = "{validation.studentId.notblank}")
   private String studentId;
 
   public Member getMemberEntity() {
